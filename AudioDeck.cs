127b90a1ca8e76c9fc09aede18be882abe7bd324
Jeremy Ortins <jortins@outlook.com>
2014-11-15 22:51:59 -0500

    Added classes that were constructed for the Quiddler application

---
+++ AudioDeck.cs
@@ -0,0 +1,254 @@
+﻿/**	
+* @file			AudioDeck.cs
+* @author		Khalid Andari & Jeremy Ortins
+* @date			2013-03-28
+* @version		1.0
+* @revisions	
+* @desc			A wrapper for a managed collection of playable sound files
+ *              Interal method calls are wrapped to ensure only loaded sounds are called
+ *              Allows implementing class to run successfully without sound
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiddlerLibrary
+{
+    /// <summary>
+    /// Private child class
+    /// </summary>
+    class Audio
+    {
+        // Member variables and accessor methods
+        private bool _loaded;
+        private SoundPlayer _sound;
+
+        public bool Loaded
+        {
+            get { return _loaded; }
+            set { _loaded = value; }
+        }
+
+        public SoundPlayer Sound
+        {
+            get { return _sound; }
+            set { _sound = value; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Audio()
+        {
+            this._loaded = false;
+            this._sound = new SoundPlayer();
+        }
+    }
+
+    public interface IAudioDeck
+    {
+        void Add(string key, string fileName);
+        void Loop(string key);
+        void Play(string key);
+        void PlaySync(string key);
+        void Remove(string key);
+        void Stop(string key);
+    }
+
+    public class AudioDeck : IAudioDeck
+    {
+        // Member variables and accessor methods
+        Dictionary<string, Audio> sounds = null;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AudioDeck()
+        {
+            try
+            {
+                sounds = new Dictionary<string, Audio>();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Adds a sound to the collection with the key specified
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="fileName"></param>
+        public void Add(string key, string fileName)
+        {
+            try
+            {
+                // Create dictionary entry
+                sounds.Add(key, new Audio());
+
+                // Load sound file
+                sounds[key].Sound = Load(fileName);
+
+                // Indicate load success
+                sounds[key].Loaded = true;
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    // Indicate file error
+                    sounds[key].Loaded = false;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a loaded SoundPlayer for the file specified
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private SoundPlayer Load(string fileName)
+        {
+            try
+            {
+                // Initialize the player
+                SoundPlayer sound = new SoundPlayer();
+
+                // Designate the file path
+                sound.SoundLocation = fileName;
+
+                // Load the file
+                sound.Load();
+
+                // Return the new sound
+                return sound;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Loops a sound from the collection with the key specified
+        /// </summary>
+        /// <param name="key"></param>
+        public void Loop(string key)
+        {
+            try
+            {
+                // Sound exists
+                if (sounds.ContainsKey(key))
+                {
+                    // File loaded
+                    if (sounds[key].Loaded)
+                    {
+                        sounds[key].Sound.PlayLooping();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Plays a sound from the collection with the key specified
+        /// </summary>
+        /// <param name="key"></param>
+        public void Play(string key)
+        {
+            try
+            {
+                // Sound exists
+                if (sounds.ContainsKey(key))
+                {
+                    // File loaded
+                    if (sounds[key].Loaded)
+                    {
+                        sounds[key].Sound.Play();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Plays a sound until completion from the collection with the key specified
+        /// </summary>
+        /// <param name="key"></param>
+        public void PlaySync(string key)
+        {
+            try
+            {
+                // Sound exists
+                if (sounds.ContainsKey(key))
+                {
+                    // File loaded
+                    if (sounds[key].Loaded)
+                    {
+                        sounds[key].Sound.PlaySync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Removes a sound from the collection with the key specified
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(string key)
+        {
+            try
+            {
+                sounds.Remove(key);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Stops a sound from the collection with the key specified
+        /// </summary>
+        /// <param name="key"></param>
+        public void Stop(string key)
+        {
+            try
+            {
+                // Sound exists
+                if (sounds.ContainsKey(key))
+                {
+                    // File loaded
+                    if (sounds[key].Loaded)
+                    {
+                        sounds[key].Sound.Stop();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,243 @@
+﻿/**	
+* @file			Logger.cs
+* @author		Khalid Andari & Jeremy Ortins
+* @date			2013-03-23
+* @version		1.0
+* @revisions	
+* @desc			A portable stream writer wrapper that incorporates various line formatting styles, timestamping and levels of logging for flexibility.
+*               Includes the implementation of the IDisposable interface
+ *              "Inspired by the Apache log4j logging library for Java" - Jeremy Ortins
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiddlerLibrary
+{
+    public enum LoggingMode
+    {
+        Debug = 4,
+        Info = 3,
+        Warning = 2,
+        Error = 1,
+    }
+
+    public interface ILogger
+    {
+        void logDebug(string message);
+        void logInfo(string message);
+        void logWarning(string message);
+        void logError(string message);
+        void setMode(LoggingMode mode);
+        void Dispose();
+    }
+
+    public class Logger : ILogger, IDisposable
+    {
+        // Member variables and accessor methods
+        private StreamWriter _logger = null;
+        private LoggingMode _loggingMode;
+        private string _logFile;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="mode"></param>
+        public Logger(string fileName, LoggingMode mode)
+        {
+            try
+            {
+                this._logFile = fileName;
+                this._loggingMode = mode;
+                this._logger = new StreamWriter(_logFile);
+            }
+            catch (FileLoadException ex)
+            {
+                throw ex;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;   
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Destructor (Finalize) method
+        /// </summary>
+        ~Logger()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Changes the level of messages logged out to file
+        /// </summary>
+        /// <param name="mode"></param>
+        public void setMode(LoggingMode mode)
+        {
+            try
+            {
+                this._loggingMode = mode;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /* ======================================================= */
+        /* =================== STREAM METHODS ==================== */
+        /* ======================================================= */
+
+        /// <summary>
+        /// Writes a debug line to the output stream file
+        /// </summary>
+        /// <param name="message"></param>
+        public void logDebug(string message)
+        {
+            try
+            {
+                // Debug, Info, Warnings & Errors
+                if (this._loggingMode == LoggingMode.Debug)
+                {
+                    this._logger.WriteLine(DateTime.Now + " -DEBUG- " + message);
+                }
+            }
+            catch (FileLoadException ex)
+            {
+                throw ex;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Writes an error line to the output stream file
+        /// </summary>
+        /// <param name="message"></param>
+        public void logError(string message)
+        {
+            try
+            {
+                // Errors only
+                if (this._loggingMode >= LoggingMode.Error)
+                {
+                    this._logger.WriteLine(DateTime.Now + " -ERROR- " + message);
+                }
+            }
+            catch (FileLoadException ex)
+            {
+                throw ex;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Writes an info line to the output stream file
+        /// </summary>
+        /// <param name="message"></param>
+        public void logInfo(string message)
+        {
+            try
+            {
+                // Info, Warnings & Errors
+                if (this._loggingMode >= LoggingMode.Info)
+                {
+                    this._logger.WriteLine(DateTime.Now + " -INFO-  " + message);
+                }
+            }
+            catch (FileLoadException ex)
+            {
+                throw ex;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Writes a warning line to the output stream file
+        /// </summary>
+        /// <param name="message"></param>
+        public void logWarning(string message)
+        {
+            try
+            {
+                // Warnings & Errors
+                if (this._loggingMode >= LoggingMode.Warning)
+                {
+                    this._logger.WriteLine(DateTime.Now + " -WARN-  " + message);
+                }
+            }
+            catch (FileLoadException ex)
+            {
+                throw ex;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /* ======================================================= */
+        /* ==================== FINALIZATION ===================== */
+        /* ======================================================= */
+
+        /// <summary>
+        /// Implementation of the IDisposable interface
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Calls the StreamWriter's dispose method
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this._logger != null)
+                {
+                    this.logInfo("Closing the logger.");
+                    this._logger.Dispose();
+                }
+            }
+        }
+
+    }
+}
